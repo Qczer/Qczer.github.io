7e8fc66e73fcf1854d8ba8076776c088bdbf677a
Qczer <119138392+Qczer@users.noreply.github.com>
2026-07-25 06:44:04 +0200

    feat: Add Github Pages deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Mateusz Kuczera",
-      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
-    },
+      title: 'Mateusz Kuczera',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    }
   },
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
-  modules: ["@nuxtjs/i18n", "@nuxt/icon"],
-  css: ["~/assets/css/tailwind.css"],
+  ssr: true,
+  nitro: {
+    preset: 'github-pages'
+  },
+  modules: ['@nuxtjs/i18n', '@nuxt/icon'],
+  css: ['~/assets/css/tailwind.css'],
   i18n: {
     locales: [
-      { code: "en", iso: "en-US", file: "en.ts", name: "English" },
-      { code: "pl", iso: "pl-PL", file: "pl.ts", name: "Polski" },
+      { code: 'en', iso: 'en-US', file: 'en.ts', name: 'English' },
+      { code: 'pl', iso: 'pl-PL', file: 'pl.ts', name: 'Polski' }
     ],
-    defaultLocale: "en",
-    strategy: "no_prefix",
-    langDir: "locales",
-    vueI18n: "./i18n.config.ts",
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    langDir: 'locales',
+    vueI18n: './i18n.config.ts',
     detectBrowserLanguage: {
       useCookie: true,
-      cookieKey: "i18n_redirected",
+      cookieKey: 'i18n_redirected',
       alwaysRedirect: true,
-      fallbackLocale: "en",
+      fallbackLocale: 'en'
     },
     compilation: {
-      strictMessage: false,
-    },
+      strictMessage: false
+    }
   },
   vite: {
-    plugins: [tailwindcss()],
-  },
-});
+    plugins: [tailwindcss()]
+  }
+})
